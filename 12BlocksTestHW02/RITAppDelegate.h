e54006780c50036d6abd695df1548fcacd8808e6
Alexander Pronin <pronin.alx@gmail.com>
2014-02-09 19:14:30 +0200

    Initial Commit

---
+++ 12BlocksTestHW02/RITAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  RITAppDelegate.h
+//  12BlocksTestHW02
+//
+//  Created by Pronin Alexander on 09.02.14.
+//  Copyright (c) 2014 Pronin Alexander. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RITAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
